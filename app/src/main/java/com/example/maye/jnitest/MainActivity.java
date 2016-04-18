62d19ec50dfb99d61ed2d2a6b4515b024dd1a28b
Silver-Shen <shenzy13@qq.com>
2016-04-18 13:31:12 +0800

    start vpn service

--- app/src/main/java/com/example/maye/jnitest/MainActivity.java
+++ app/src/main/java/com/example/maye/jnitest/MainActivity.java
@@ -6,33 +6,111 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
 public class MainActivity extends AppCompatActivity {
+    private Thread IVIthread; //native C thread
+    private Thread BackGroundthread;
+    private IpPacket ip_packet;
+    //int flag = 0;
+    String ipHandleName = "/tmp/myfifo";
+    String statsHandleName = "/tmp/myfifo_stats";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
 
-        Intent intent = VpnService.prepare(this);
-        if (intent != null) {
-            startActivityForResult(intent, 0);
-        } else {
-            onActivityResult(0, RESULT_OK, null);
-        }
+        //create and start C thread
+        IVIthread = new Thread(){
+            @Override
+            public void run(){
+                IVI();
+            }
+        };
+        IVIthread.start();
 
+        //create and start background thread
+        BackGroundthread = new Thread(){
+            @Override
+            public void run(){
+                //loop reading for ip info
+                do{
+                    ip_packet = readIpHandle();
+                }while (ip_packet == null);
+
+                //create vpnService
+                Intent intent = VpnService.prepare(MainActivity.this);
+                if (intent != null) {
+                    startActivityForResult(intent, 0);
+                } else {
+                    onActivityResult(0, RESULT_OK, null);
+                }
+            }
+        };
+        BackGroundthread.start();
+        //Demo text setting
         TextView textView = new TextView(this);
         textView.setText(StringFromJNI());
         setContentView(textView);
     }
 
+
     protected void onActivityResult(int request, int result, Intent data) {
         if (result == RESULT_OK) {
             Intent intent = new Intent(this, MyVpnService.class);
+            //pass information which vpn service need
+            intent.putExtra("ip", ip_packet.ipAddress);
+            intent.putExtra("route", ip_packet.route);
+            intent.putExtra("DNS1", ip_packet.DNS1);
+            intent.putExtra("DNS2", ip_packet.DNS2);
+            intent.putExtra("DNS3", ip_packet.DNS3);
             startService(intent);
         }
     }
 
+    protected IpPacket readIpHandle(){
+        File file = new File(ipHandleName);
+        while (!file.exists()){
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        IpPacket temp_ip_packet = null;
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            BufferedInputStream in = new BufferedInputStream(fileInputStream);
+            byte buf[] = new byte[1024];
+            int readLen = in.read(buf, 0, 1024);
+            if (readLen > 0){
+                String ipDataInfo = new String(buf);
+                String piece[] = ipDataInfo.split(" ");
+                temp_ip_packet = new IpPacket();
+                temp_ip_packet.ipAddress = piece[0];
+                temp_ip_packet.route = piece[1];
+                temp_ip_packet.DNS1 = piece[2];
+                temp_ip_packet.DNS2 = piece[3];
+                temp_ip_packet.DNS3 = piece[4];
+            }
+            in.close();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return temp_ip_packet;
+    }
+
+    class IpPacket{
+        String ipAddress;
+        String route;
+        String DNS1,DNS2,DNS3;
+    }
+
+    //native JNI declaration
     public native String StringFromJNI();
+    public native void IVI();
     static {
         System.loadLibrary("jnitest");
     }
--- app/src/main/java/com/example/maye/jnitest/MyVpnService.java
+++ app/src/main/java/com/example/maye/jnitest/MyVpnService.java
@@ -4,83 +4,54 @@ import android.content.Intent;
 import android.net.VpnService;
 import android.os.ParcelFileDescriptor;
 
-import java.io.FileInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
-import java.net.InetSocketAddress;
-import java.nio.channels.DatagramChannel;
 
 /**
  * Created by Silver_Bullet on 16/4/17.
  */
 public class MyVpnService extends VpnService {
-    private Thread mThread;
     private ParcelFileDescriptor mInterface;
-    //a. Configure a builder for the interface.
+    String ipHandleName = "/tmp/myfifo";
+    //Configure a builder for the interface.
     Builder builder = new Builder();
-
     // Services interface
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        // Start a new session by creating a new thread.
-        mThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    //a. Configure the TUN and get the interface.
-                    mInterface = builder.setSession("MyVPNService")
-                            .addAddress("192.168.0.1", 24)
-                            .addDnsServer("8.8.8.8")
-                            .addRoute("0.0.0.0", 0).establish();
-                    //b. Packets to be sent are queued in this input stream.
-                    FileInputStream in = new FileInputStream(
-                            mInterface.getFileDescriptor());
-                    //b. Packets received need to be written to this output stream.
-                    FileOutputStream out = new FileOutputStream(
-                            mInterface.getFileDescriptor());
-                    //c. The UDP channel can be used to pass/get ip package to/from server
-                    DatagramChannel tunnel = DatagramChannel.open();
-                    // Connect to the server, localhost is used for demonstration only.
-                    tunnel.connect(new InetSocketAddress("127.0.0.1", 8087));
-                    //d. Protect this socket, so package send by it will not be feedback to the vpn service.
-                    protect(tunnel.socket());
-                    //e. Use a loop to pass packets.
-                    while (true) {
-                        //get packet with in
-                        //put packet to tunnel
-                        //get packet form tunnel
-                        //return packet with out
-                        //sleep is a must
-                        Thread.sleep(100);
-                    }
-
-                } catch (Exception e) {
-                    // Catch any exception
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        if (mInterface != null) {
-                            mInterface.close();
-                            mInterface = null;
-                        }
-                    } catch (Exception e) {
-
-                    }
-                }
-            }
-
-        }, "MyVpnRunnable");
-
+        //a. Configure the TUN and get the interface.
+        String ipAddress = intent.getStringExtra("ip");
+        String route = intent.getStringExtra("route");
+        String dns = intent.getStringExtra("DNS1");
+        mInterface = builder.setSession("MyVPNService")
+                .addAddress(ipAddress, 24)
+                .addDnsServer(dns)
+                .addRoute(route, 0).establish();
+        try{
+            File file = new File(ipHandleName);
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            BufferedOutputStream out = new BufferedOutputStream(fileOutputStream);
+            //transfer int to byte[]
+            int fd = mInterface.getFd();
+            ByteArrayOutputStream boutput = new ByteArrayOutputStream();
+            DataOutputStream doutput = new DataOutputStream(boutput);
+            doutput.writeInt(fd);
+            byte[] buf = boutput.toByteArray();
+            out.write(buf, 0, buf.length);
+            out.flush();
+            out.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         //start the service
-        mThread.start();
         return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
         // TODO Auto-generated method stub
-        if (mThread != null) {
-            mThread.interrupt();
-        }
         super.onDestroy();
     }
 }
