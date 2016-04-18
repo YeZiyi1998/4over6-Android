985070a3837e7371e4ebd2ddefe30eaa274e443c
Silver-Shen <shenzy13@qq.com>
2016-04-18 10:38:40 +0800

    test vpn

--- app/src/main/java/com/example/maye/jnitest/MainActivity.java
+++ app/src/main/java/com/example/maye/jnitest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.maye.jnitest;
 
+import android.content.Intent;
+import android.net.VpnService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -10,9 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
+
+        Intent intent = VpnService.prepare(this);
+        if (intent != null) {
+            startActivityForResult(intent, 0);
+        } else {
+            onActivityResult(0, RESULT_OK, null);
+        }
+
         TextView textView = new TextView(this);
         textView.setText(StringFromJNI());
         setContentView(textView);
+    }
+
+    protected void onActivityResult(int request, int result, Intent data) {
+        if (result == RESULT_OK) {
+            Intent intent = new Intent(this, MyVpnService.class);
+            startService(intent);
+        }
     }
 
     public native String StringFromJNI();
