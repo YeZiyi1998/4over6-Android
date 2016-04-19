0e92b2c68a531099806b262e8febf26bf9a2950b
Silver-Shen <shenzy13@qq.com>
2016-04-19 21:01:35 +0800

    add GUI

--- app/src/main/java/com/example/maye/jnitest/MainActivity.java
+++ app/src/main/java/com/example/maye/jnitest/MainActivity.java
@@ -2,44 +2,73 @@ package com.example.maye.jnitest;
 
 import android.content.Intent;
 import android.net.VpnService;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.sql.Time;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
     private Thread IVIthread; //native C thread
-    private Thread BackGroundthread;
+    private Thread BackGroundthread; //background thread
     private IpPacket ip_packet;
-    //int flag = 0;
+    private  GuiInfo guiInfo;
+    private Date startTime;
+    private int isStart = 0;
     String ipHandleName = "/data/data/com.example.maye.jnitest/myfifo";
     String statsHandleName = "/data/data/com.example.maye.jnitest/myfifo_stats";
     private final String TAG = "MainActivity";
-
+    //user interface components
+    private Button startButton;
+    private TextView time_info;
+    private TextView speed_info;
+    private TextView send_info;
+    private TextView receive_info;
+    private TextView ipv4_addr;
+    private TextView ipv6_addr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //create and start C thread
+        setContentView(R.layout.activity_main);
+        //mapping GUI to variables
+        startButton = (Button) findViewById(R.id.start_service);
+        time_info = (TextView) findViewById(R.id.time_duration);
+        speed_info = (TextView) findViewById(R.id.speed_info);
+        send_info = (TextView) findViewById(R.id.send_info);
+        receive_info = (TextView) findViewById(R.id.receive_info);
+        ipv4_addr = (TextView) findViewById(R.id.ipv4_addr);
+        ipv6_addr = (TextView) findViewById(R.id.ipv6_addr);
+        guiInfo = new GuiInfo();
+        //create C thread
         IVIthread = new Thread(){
             @Override
             public void run(){
                 IVI();
             }
         };
-        IVIthread.start();
 
-        //create and start background thread
+
+        //create Background thread
         BackGroundthread = new Thread(){
             @Override
             public void run(){
                 //read for ip info
                 ip_packet = readIpHandle();
-
+                guiInfo.ipv4_addr = ip_packet.ipAddress;
                 //create vpnService
                 Intent intent = VpnService.prepare(MainActivity.this);
                 if (intent != null) {
@@ -47,13 +76,29 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     onActivityResult(0, RESULT_OK, null);
                 }
+                //start timer for GUI refreshing
+                startTime = new Date();
+                Timer timer = new Timer();
+                timer.schedule(new MyTask(), 500, 1000);//500ms delay and 1s cycle
             }
         };
-        BackGroundthread.start();
-        //Demo text setting
-        TextView textView = new TextView(this);
-        textView.setText(StringFromJNI());
-        setContentView(textView);
+
+        //push button to start service
+        startButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                guiInfo.ipv6_addr = getLocalIpAddress();
+                if (isStart == 0 && guiInfo.ipv6_addr != null) {
+                    IVIthread.start();
+                    BackGroundthread.start();
+                    isStart = 1;
+                }else if (guiInfo.ipv6_addr == null){
+                    Log.d(TAG, "network unavailable!");
+                }
+            }
+        });
     }
 
 
@@ -110,6 +155,81 @@ public class MainActivity extends AppCompatActivity {
         String route;
         String DNS1,DNS2,DNS3;
         String socket;
+    }
+
+    class GuiInfo{
+        String time_duration;
+        String speed_info;
+        String send_info;
+        String receive_info;
+        String ipv4_addr;
+        String ipv6_addr;
+    }
+
+    //get Local Network information
+    public String getLocalIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface
+                    .getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf
+                        .getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        return inetAddress.getHostAddress().toString();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    //timer routine
+    class MyTask extends java.util.TimerTask{
+        public void run(){
+            File file = new File(statsHandleName);
+            while (!file.exists()){
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                FileInputStream fileInputStream = new FileInputStream(file);
+                BufferedInputStream in = new BufferedInputStream(fileInputStream);
+                byte buf[] = new byte[1024];
+                int readLen = in.read(buf, 0, 1024);
+                if (readLen > 0){
+                    String ipStatInfo = new String(buf);
+                    String piece[] = ipStatInfo.split(" ");
+                    guiInfo.send_info = "Send: " + piece[0] + " bytes/" + piece[1] + " packets";
+                    guiInfo.receive_info = "Receive: " + piece[2] + " bytes/" + piece[3] + " packets";
+                    guiInfo.time_duration = new Long((new Date()).getTime()-startTime.getTime()).toString();
+                    refreshGUI();
+                }
+                in.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    protected void refreshGUI(){
+        Log.v(TAG, "refreshing");
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                time_info.setText(guiInfo.time_duration);
+                speed_info.setText("Unknown!");
+                send_info.setText(guiInfo.send_info);
+                receive_info.setText(guiInfo.receive_info);
+                ipv4_addr.setText(guiInfo.ipv4_addr);
+                ipv6_addr.setText(guiInfo.ipv6_addr);
+            }
+        });
     }
 
     //native JNI declaration
